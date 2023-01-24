ba4c34d75d0196cb2c363b76d21f7bccbb071bb5
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-24 20:00:28 -0300

    Projeto Finalizado

---
+++ CalculoPreco/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CalculoPreco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double produto, desconto, valorFinal;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Informe o Valor do produto: ");
+            produto= Double.Parse(Console.ReadLine());
+            Console.ResetColor();
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Informe o Valor do desconto: ");
+            desconto = Double.Parse(Console.ReadLine());
+            Console.ResetColor();
+            Console.Clear();
+
+            valorFinal = produto - desconto;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("O valor a ser pago: "+ valorFinal);
+            Console.ResetColor();
+
+            Console.ReadLine();
+        }
+    }
+}
